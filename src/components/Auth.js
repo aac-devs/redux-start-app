52ade14ba72b820d7e7f702aafe6e66928b48a9e
Andrés <aac.devs@gmail.com>
2021-06-26 14:26:02 -0300

    Section 18 finished

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,40 @@
-import classes from './Auth.module.css';
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+
+    const isValidEmail = enteredEmail.trim().includes("@");
+    const isValidPassword = enteredPassword.trim().length >= 6;
+
+    if (!isValidEmail || !isValidPassword) {
+      return;
+    }
+
+    dispatch(authActions.login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input ref={emailRef} type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input ref={passwordRef} type="password" id="password" />
           </div>
           <button>Login</button>
         </form>
